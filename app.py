fa348d2987f2761c512891c60552059a8d325c87
Luciane de Castro <luciane.castro@gmail.com>
2024-10-14 01:06:44 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,55 +1,22 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from flask import Flask, render_template, jsonify, send_from_directory
+import json
 
 app = Flask(__name__)
-CORS(app)
 
-# Credenciais da API do Google (ajuste o caminho para o arquivo JSON)
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\lucia\desempenhotecnicosV2\disco-freedom-433500-h0-d619a1073895.json', scope) 
-client = gspread.authorize(creds)
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-# Link da Planilha no Google Drive (substitua pelo seu link)
-spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1qsUHg92SYqbpufM9nTIZuBhkNHmgM-uL/edit?usp=drive_link&ouid=111070395629463126436&rtpof=true&sd=true'
+@app.route("/anos")
+def get_anos():
+    with open('dados.json', 'r') as f:
+        data = json.load(f)
+    anos = sorted(set([item["Ano"] for item in data["geral"]]))
+    return jsonify(anos) 
 
-# Abrir a Planilha
-spreadsheet = client.open_by_url(spreadsheet_url)
+@app.route('/dados.json')
+def get_dados_json():
+    return send_from_directory('.', 'dados.json')
 
-# Função para ler os dados de uma aba
-def ler_dados_da_aba(aba_nome):
-    worksheet = spreadsheet.worksheet(aba_nome)
-    dados = worksheet.get_all_records()
-    return dados
-
-# Rota /api/geral
-@app.route('/api/geral')
-def api_geral():
-    ano = request.args.get('ano')
-    dados = ler_dados_da_aba('Geral')
-    if ano:
-        dados = [d for d in dados if d['Ano'] == int(ano)]
-    return jsonify(dados)
-
-# Rota /api/desempenho
-@app.route('/api/desempenho')
-def api_desempenho():
-    ano = request.args.get('ano')
-    dados = ler_dados_da_aba('Desempenho')
-    if ano:
-        dados = [d for d in dados if any(str(d.get(f'Ano_{i}')) == ano for i in range(1, 5))]
-    return jsonify(dados)
-
-# Rota /api/detalhado
-@app.route('/api/detalhado')
-def api_detalhado():
-    ano = request.args.get('ano')
-    dados = ler_dados_da_aba('Detalhado')
-    if ano:
-        dados = [d for d in dados if d['Ano'] == int(ano) and int(d.get('Jogos', 0)) != 0]
-    return jsonify(dados)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(debug=True)
